b00353b036a52420c379fa74d42faa78c9e4b39f
JanHakanViktor <janhakanv@gmail.com>
2025-09-02 14:00:21 +0200

    Reverted styling that previously got deleted.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import TodoList from "./components/TodoList.tsx";
+import "./main.module.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <TodoList />
   </React.StrictMode>
 );
